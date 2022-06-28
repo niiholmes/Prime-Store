9ba8ab9a887ff40310094ea43d1648ebb25d2a63
niiholmes <iamniiholmes@gmail.com>
2022-06-28 20:20:39 +0000

    ui update

--- pages/cart.js
+++ pages/cart.js
@@ -2,7 +2,6 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React, { useContext } from 'react';
 import { Store } from '../utils/Store';
-import { XCircleIcon } from '@heroicons/react/outline';
 
 export default function CartPage() {
   const { state, dispatch } = useContext(Store);
@@ -15,51 +14,43 @@ export default function CartPage() {
   };
   return (
     <div>
-      <h1 className="mb-4 text-xl">shopping cart</h1>
+      <Link className="mt-7 text-xs font-bold" href="/">
+        <a className="">back to shopping</a>
+      </Link>
+      <h1 className="mb-10 text-sm text-right mr-5 mt-5 ">Review your Bag</h1>
       {cartItems.length === 0 ? (
         <div>
-          Cart is empty. <Link href="/">Go shopping</Link>
+          Bag is empty. <Link href="/">Go shopping</Link>
         </div>
       ) : (
         <div className="grid md:grid-cols-4 md:gap-5">
-          <div className="overflow-x-auto md:col-span-3">
-            <table className="min-w-full">
-              <thead className="border-b">
-                <tr>
-                  <th className="px-5 text-left">Item</th>
-                  <th className="px-5 right">Quantity</th>
-                  <th className="px-5 right">Price</th>
-                  <th className="px-5">Action</th>
-                </tr>
-              </thead>
-              <tbody>
-                {cartItems.map((item) => (
-                  <tr key={item.slug} className="border-b">
-                    <td>
-                      <Link href={`/product/${item.slug}`}>
-                        <a className="flex items-center">
-                          <Image
-                            src={item.image}
-                            alt={item.name}
-                            width={50}
-                            height={50}
-                          ></Image>
-                          &nbsp;
-                          <h1 className="text-xs">{item.product_name}</h1>
-                        </a>
-                      </Link>
-                    </td>
-                    <td className="p-5 text-right">{item.quantity}</td>
-                    <td className="p-5 text-right">${item.price}</td>
-                    <td className="p-5 text-center">
-                      <button onClick={() => removeItemHandler(item)}>
-                        <XCircleIcon className="h-5 w-5"></XCircleIcon>
-                      </button>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
+          <div>
+            {cartItems.map((item) => (
+              <div key={item.slug} className="">
+                <Link href={`/product/${item.slug}`}>
+                  <a className="">
+                    <h1 className="text-sm mb-7">{item.product_name}</h1>
+
+                    <Image
+                      src={item.image}
+                      alt={item.name}
+                      width={600}
+                      height={404}
+                    ></Image>
+                  </a>
+                </Link>
+                <div className="flex justify-evenly mt-5">
+                  <h4 className=" text-sm f"> {item.quantity}</h4>
+                  <h4 className=" text-sm ">$ {item.price}</h4>
+                  <h4  className=" mr-5  text-sm font-bold mb-7 " onClick={() => removeItemHandler(item)}>
+                    Remove item
+                  </h4>
+                </div>
+                
+                 
+                
+              </div>
+            ))}
           </div>
         </div>
       )}
