ec968ee37042056f25f94b3e84ae1250070665c0
niiholmes <iamniiholmes@gmail.com>
2022-06-30 11:51:16 +0000

    ui update

--- components/pins.js
+++ components/pins.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 
-export default function Pins({ img, dimensions, item, size, width, height }) {
+export default function Pins({ img, item, size, width, height }) {
   return (
     <>
       <div
