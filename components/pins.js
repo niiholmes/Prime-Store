0a8a28323e9cade0d9131604d2c7f5ea097dfedd
niiholmes <iamniiholmes@gmail.com>
2022-06-30 22:16:10 +0000

    redesigning home page

--- components/pins.js
+++ components/pins.js
@@ -2,23 +2,23 @@ import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 
-export default function Pins({ img, item,}) {
+export default function Pins({ img, item, width, height}) {
   return (
     <>
-      <div className=" my-5 mx-1 p-0 rounded-2xl  bg-white drop-shadow-sm  hover:drop-shadow-xl liftOffAnimation">
+      <div className=" mt-5 mx-1 p-0 rounded-2xl  drop-shadow-sm  hover:drop-shadow-xl ">
         <Link href="#">
           <a>
             <Image
               src={img}
-              width={300}
-              height={390}
+              width={width}
+              height={height}
               alt="categories"
               layout='responsive'
               className="object-cover rounded-2xl "
             />
           </a>
         </Link>
-        <h4 className="text-left text-xs relative top-6 pl-2">{item}</h4>
+        <h4 className="text-left text-xs relative top-1 pl-2">{item}</h4>
       </div>
     </>
   );
--- utils/data.js
+++ utils/data.js
@@ -4,12 +4,16 @@ const data = {
       id: '1',
       img: '/assets/sneaks.JPG',
       item: 'Sneakers',
+      width: 300,
+      height: 390
 
     },
     {
       id: '2',
       img: '/assets/clothes.JPG',
       item: 'Apparels',
+      width: 300,
+      height: 460
 
     
     },
@@ -17,18 +21,24 @@ const data = {
       id: '3',
       img: '/assets/watch.JPG',
       item: 'Watches',
+      width: 300,
+      height: 500
 
     },
     {
       id: '4',
       img: '/assets/phones.JPG',
       item: 'Phones',
+      width: 300,
+      height: 390
  
     },
     {
       id: '5',
       img: '/assets/lap.JPG',
       item: 'Laptops',
+      width: 300,
+      height: 460
 
  
     },
@@ -36,6 +46,8 @@ const data = {
       id: '6',
       img: '/assets/tv.JPG',
       item: 'TVs',
+      width: 300,
+      height: 500
 
  
     },
@@ -43,6 +55,8 @@ const data = {
       id: '7',
       img: '/assets/accessories.JPG',
       item: 'Accesories',
+      width: 300,
+      height: 390
 
    
     },
@@ -50,6 +64,8 @@ const data = {
       id: '8',
       img: '/assets/bks.JPG',
       item: 'Books',
+      width: 300,
+      height: 460
 
 
     },
@@ -57,6 +73,8 @@ const data = {
       id: '9',
       img: '/assets/fav.JPG',
       item: 'Fruits & Vegetables',
+      width: 300,
+      height: 500
 
 
     },
